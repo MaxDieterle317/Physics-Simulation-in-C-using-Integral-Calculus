d2d6b594e534263444bf3ce58a2a9db53fc5f0b9
Max Dieterle <Maxdieterle17@gmail.com>
2025-12-07 21:45:23 +0000

    added main and particle object

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <chrono> //for the clock
+#include <vector>
+#include "objects.h"
+
+using namespace std;
+
+
+int main(){
+    using clock = chrono::high_resolution_clock; //the clock we will be using is the high resolution clock from the chrono library
+    using ms = chrono::duration<double, milli>; //time will be measured using milliseconds
+
+    const int particleCount = 1000; //how much of something do we have?
+    const int numSteps = 1000; //how many steps the sim has?
+    const double dt = 0.01; //change in time
+
+    vector<Particle> particles(particleCount); //create and set particles
+    for (int i = 0; i < particles.size(); i++) {
+        particles[i].setPos(0.0);
+        particles[i].setVel(0.0);
+        particles[i].setAcc(1.0);
+    }
+
+    auto start = clock::now(); //start the clock
+
+    //placeholder simulation loop
+    for (int timestep = 0; timestep < numSteps; timestep++) {
+        for (int j = 0; j < particles.size(); j++){
+            Particle& p = particles[j];
+            double x = p.getPos(); //simpler variables
+            double v = p.getVel();
+            double a = p.getAcc();
+
+            v += a * dt; //equations for velocity and position
+            x += v * dt;
+
+            p.setVel(v); //set new position and velocity for next step
+            p.setPos(x);
+        }
+    }
+
+    auto end = clock::now(); //stop clock
+
+    ms timeElapsed = end - start; //how much time passed?
+
+    cout << "Simulated " << particleCount << " particles for " << numSteps << " steps in " << timeElapsed.count() << " ms\n";
+
+    return 0;
+}
+
---
+++ objects.h
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+class Particle {
+    private:
+        double position;
+        double velocity;
+        double acceleration;
+    public:
+        Particle(double position = 0.0, double velocity = 0.0, double acceleration = 0.0) //constructor
+            : position(position), velocity(velocity), acceleration(acceleration) {}
+
+        double getPos() const {return position;} //gets
+        double getVel() const {return velocity;}
+        double getAcc() const {return acceleration;}
+
+        void setPos(double newPos) { position = newPos; } //sets
+        void setVel(double newVel) { velocity = newVel; }
+        void setAcc(double newAcc) { acceleration = newAcc; }
+ 
+        void update(double dt) { //helper to update position and velocity based on time
+        velocity += acceleration * dt;  
+        position += velocity * dt;  
+        }
+};
