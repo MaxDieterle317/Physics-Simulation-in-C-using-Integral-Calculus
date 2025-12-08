76ec02135916301f8fa27af16667680e2d53ed30
Max Dieterle <Maxdieterle17@gmail.com>
2025-12-08 00:18:04 +0000

    input

--- main.cpp
+++ main.cpp
@@ -14,17 +14,54 @@ int main(){
     using clock = chrono::high_resolution_clock; //the clock we will be using is the high resolution clock from the chrono library
     using ms = chrono::duration<double, milli>; //time will be measured using milliseconds
 
-    const int particleCount = 1000; //how much of something do we have?
-    const int numSteps = 1000; //how many steps the sim has?
-    const int samplesPerStep = 100; //how many samples are we running in each step?
-    const double dt = 0.01; //change in time
+    int particleCount;       // how many particles?
+    int numSteps;            // how many time steps?
+    const double dt = 0.01;  // change in time
+    double acceleration; 
+    int samplesPerStep; //how many samples are we running in each step?
 
+    do {
+        cout << "Enter number of particles (must be greater than 0): ";
+        cin >> particleCount;
+        if (particleCount <= 0) {
+            cout << "Invalid input. Please enter a positive integer.\n";
+        }
+    } while (particleCount <= 0);
+
+    do {
+        cout << "Enter number of time steps (must be greater than 0): ";
+        cin >> numSteps;
+        if (numSteps <= 0) {
+            cout << "Invalid input. Please enter a positive integer.\n";
+        }
+    } while (numSteps <= 0);
+    
+    cout << "Enter particle acceleration: ";
+    cin >> acceleration;
+
+    do {
+        cout << "Monte Carlo integration takes the average of a certain number of samples.\n";
+        cout << "Enter sample count (must be greater than 0): ";
+        cin >> samplesPerStep;
+        if (samplesPerStep <= 0) {
+            cout << "Invalid input. Please enter a positive integer.\n";
+        }
+    } while (samplesPerStep <= 0);
+    
     vector<Particle> particles(particleCount); //create and set particles
     for (int i = 0; i < particles.size(); i++) {
         particles[i].setPos(0.0);
         particles[i].setVel(0.0);
-        particles[i].setAcc(1.0);
+        particles[i].setAcc(acceleration);
     }
+
+    cout << "\n----- Simulation Settings -----\n"; //adds some flair to the UI
+    cout << "Particles:      " << particleCount << '\n';
+    cout << "Time steps:     " << numSteps << '\n';
+    cout << "dt:             " << dt << '\n';
+    cout << "Acceleration:   " << acceleration << '\n';
+    cout << "Samples/step:   " << samplesPerStep << '\n';
+    cout << "-------------------------------\n\n";
 
     auto start = clock::now(); //start the clock
 
@@ -49,11 +86,30 @@ int main(){
     auto end = clock::now(); //stop clock
 
     ms timeElapsed = end - start; //how much time passed?
-    
-    cout << "Final v of particle 0: " << particles[0].getVel() << '\n';
-    cout << "Final x of particle 0: " << particles[0].getPos() << '\n';
 
-    cout << "Simulated " << particleCount << " particles for " << numSteps << " steps in " << timeElapsed.count() << " ms\n";
+    double T = numSteps * dt; //total time
+    double exactVel = acceleration * T; //exact velocity
+    double exactPos = 0.5 * acceleration * T * T; //exact position
+
+    double simVel = particles[0].getVel();
+    double simPos = particles[0].getPos();
+
+    double velError = simVel - exactVel;
+    double posError = simPos - exactPos;
+
+    cout << "\n----------- Results -----------\n";
+    cout << "Exact final velocity:   " << exactVel << '\n';
+    cout << "Simulated final velocity: " << simVel << '\n';
+    cout << "Velocity error:         " << velError << "\n\n";
+
+    cout << "Exact final position:   " << exactPos << '\n';
+    cout << "Simulated final position: " << simPos << '\n';
+    cout << "Position error:         " << posError << "\n";
+    cout << "-------------------------------\n\n";
+
+
+    cout << "Simulated " << particleCount << " particles for "
+        << numSteps << " steps in " << timeElapsed.count() << " ms\n\n";
 
     return 0;
 }
